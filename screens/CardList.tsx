ebb32b8705152024aca7a244b1ea36033682164f
Vishal Jangid <vishaljangid2@outlook.com>
2024-11-28 02:34:09 +0700

    Updated to show all cards by customer

--- screens/CardList.tsx
+++ screens/CardList.tsx
@@ -1,32 +1,100 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { ScreenContent } from 'components/ScreenContent';
-import { StyleSheet, View } from 'react-native';
+import { Image, SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
 
-import { Button } from '../components/Button';
 import { RootStackParamList } from '../navigation';
+
+import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import useOmise from 'useOmise';
+import { Feather } from '@expo/vector-icons';
+import { getBrandLogo } from 'Constants/GetBrandLogo';
 
 type OverviewScreenNavigationProps = StackNavigationProp<RootStackParamList, 'CardList'>;
 
-export default function Overview() {
-  const navigation = useNavigation<OverviewScreenNavigationProps>();
+export default function Overview({ route }) {
+  // Use Custom Hooks
+  const { getCardsForCustomer } = useOmise();
 
+  // Navigation
+  const navigation = useNavigation<OverviewScreenNavigationProps>();
   navigation.setOptions({
-    headerShown: false
-  })
+    headerShown: false,
+  });
+
+  // State
+  const [cards, setAllCards] = useState([]);
+
+  // Hooks
+  useEffect(() => {
+    const params = route.params;
+    if (params.customerId) {
+      const fetchResponse = async () => {
+        const res = await getCardsForCustomer(params.customerId).then((data) => {
+          setAllCards(data);
+          console.log('CARDS', cards);
+        });
+      };
+      fetchResponse();
+    } else console.log('No customer params recv.');
+  }, []);
 
   return (
-    <View className='flex-1'>
-      <ScreenContent path="screens/overview.tsx" title="Overview" />
-      <Button
-        onPress={() =>
-          navigation.navigate('AddCard', {
-            name: 'Dan',
-          })
-        }
-        title="Show Details"
-      />
-    </View>
+    <SafeAreaView>
+      <View className="gap-5 p-5">
+        {/* Header */}
+        <View className="flex-row items-center justify-between">
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Feather name="arrow-left" size={25} />
+          </TouchableOpacity>
+          <Text className="text-lg font-bold">Cards</Text>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Feather name="plus" size={30} />
+          </TouchableOpacity>
+        </View>
+        {cards !== undefined ? (
+          <View className="flex h-full gap-5">
+            <ScrollView contentContainerStyle={{ flexGrow: 1, gap: 20 }}>
+              {cards.map((item, index) => (
+                <View
+                  key={index}
+                  className="elevation-xl h-56 w-full gap-3 rounded-xl bg-white p-9 shadow shadow-gray-300">
+                  <Image className="h-10 w-20" source={getBrandLogo(item.brand)} />
+                  <View className="mb-2 flex-row gap-5">
+                    <Text className="text-2xl font-bold text-gray-600">••••</Text>
+                    <Text className="text-2xl font-bold text-gray-600">••••</Text>
+                    <Text className="text-2xl font-bold text-gray-600">••••</Text>
+                    <Text className="text-2xl text-gray-600">{item.last_digits}</Text>
+                  </View>
+                  <View className="flex-row justify-between">
+                    <View className="gap-3">
+                      <Text className="text-gray-400">Name on Card</Text>
+                      <Text className="text-lg font-bold">{item.name}</Text>
+                    </View>
+                    <View className="gap-3">
+                      <Text className="text-gray-400">Expires</Text>
+                      <Text className="text-lg font-bold">
+                        {item.expiration_month}/{item.expiration_year}
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              ))}
+            </ScrollView>
+          </View>
+        ) : (
+          <View className="h-[90%] items-center justify-center">
+            <Text>No Cards found.</Text>
+            <Text>We recommend adding card for easy payment</Text>
+            <TouchableOpacity
+              className=""
+              onPress={() => navigation.navigate('AddCard', { params: route.params.customerId })}>
+              <Text className="text-primary text-xl font-bold">Add new card</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
