0e07b7a7305ad231b059f4bcbbcaa8fb9aa171c8
Vishal Jangid <vishaljangid2@outlook.com>
2024-11-27 23:07:44 +0700

    Tailwind extend to add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#49D6D8"
+      }
+    },
   },
   plugins: [],
 };
