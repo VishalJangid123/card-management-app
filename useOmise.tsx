e3c66e65eb12c8cc8af3942d670b6e2447d1d0c9
Vishal Jangid <vishaljangid2@outlook.com>
2024-11-28 02:31:39 +0700

    Added get all cards by customer id / create token by saving card and attach to customer

--- useOmise.tsx
+++ useOmise.tsx
@@ -12,11 +12,25 @@ type OmiseCustomerResponse = {
   email: string;
 };
 
+type OmiseCard = {
+  id: string;
+  last_digits: string;
+  brand: string;
+};
+
+type OmiseCardToken = {
+  id: string;
+  card: {
+    brand: string;
+    last_digits: string;
+  };
+};
+
 const publicKey = ''; // Add key
 const secretKey = '';
 
 const useOmise = () => {
-  const { customer, addCustomer } = useCustomer();
+  const { addCustomer } = useCustomer();
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -46,8 +60,90 @@ const useOmise = () => {
     }
   };
 
+  const getCardsForCustomer = async (customerId): Promise<OmiseCard[]> => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get<OmiseResponse<OmiseCard[]>>(
+        `https://api.omise.co/customers/${customerId}/cards`,
+        {
+          auth: {
+            username: secretKey,
+            password: '',
+          },
+        }
+      );
+      return response.data.data;
+    } catch (err) {
+      console.log(err)
+      setError(err.response?.data?.message || 'An error occurred');
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const createCardToken = async (
+    name: string,
+    cardNumber: string,
+    expiryMonth: string,
+    expiryYear: string,
+    cardCvc: string,
+    customerId : string
+  ): Promise<OmiseCardToken> => {
+    setLoading(true);
+    setError(null);
+    console.log("create token")
+    try {
+      const response = await axios.post<OmiseResponse<OmiseCardToken>>(
+        'https://vault.omise.co/tokens',
+        {
+          card: {
+            name: name,
+            number: cardNumber,
+            expiration_month: expiryMonth,
+            expiration_year: expiryYear,
+            security_code: cardCvc,
+          },
+        },
+        {
+          auth: {
+            username: publicKey,
+            password: '',
+          },
+        }
+      );
+      const tokenId = response.data.id;
+      console.log("token", tokenId)
+      const url = `https://api.omise.co/customers/${customerId}`
+      console.log("URL ",url)
+      const result = await axios.patch(
+        url,
+        {card: tokenId},
+        {
+          auth: {
+            username: secretKey,
+            password: '',
+          },
+        })
+
+      console.log(result.data)
+      // Attach to customer
+      return result.data;
+    } catch (err) {
+      console.log(err)
+      console.log(err.response)
+      setError(err.response?.data?.message || 'An error occurred');
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return {
     createCustomer,
+    getCardsForCustomer,
+    createCardToken,
     error,
     loading,
   };
