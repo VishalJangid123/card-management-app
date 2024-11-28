303c1c6c45afcf32cc2b196f7a2e03aca487e9eb
Vishal Jangid <vishaljangid2@outlook.com>
2024-11-29 01:26:53 +0700

    async Font loading

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -6,6 +6,9 @@ import Details from '../screens/AddCard';
 import Overview from '../screens/CardList';
 import { CustomerProvider } from 'context/CustomerContext';
 import CustomerList from 'screens/CustomerList';
+import { ActivityIndicator, Text } from 'react-native';
+import * as Font from 'expo-font';
+import { useEffect, useState } from 'react';
 
 export type RootStackParamList = {
   CustomerList: undefined;
@@ -16,21 +19,46 @@ export type RootStackParamList = {
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
+  const [fontsLoaded] = Font.useFonts({
+    'FCSubjectRoundedNoncml-Bold': require('../assets/fonts/FC_Subject_Rounded_Bold.otf'),
+    FCr: require('../assets/fonts/FC_Subject_Rounded_Regular.otf'),
+  });
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          'FCSubjectRoundedNoncml-Bold': require('../assets/fonts/FC_Subject_Rounded_Bold.otf'),
+        });
+        setFontLoaded(true);
+      } catch (error) {
+        console.error('Error loading font', error);
+      }
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontLoaded) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
   return (
     <CustomerProvider>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="CustomerList">
-      <Stack.Screen name="CustomerList" component={CustomerList} />
-      <Stack.Screen name="CardList" component={Overview} />
-        <Stack.Screen
-          name="AddCard"
-          component={Details}
-          options={({ navigation }) => ({
-            headerLeft: () => <BackButton onPress={navigation.goBack} />,
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="CustomerList">
+          <Stack.Screen name="CustomerList" component={CustomerList} />
+          <Stack.Screen name="CardList" component={Overview} />
+          <Stack.Screen
+            name="AddCard"
+            component={Details}
+            options={({ navigation }) => ({
+              headerLeft: () => <BackButton onPress={navigation.goBack} />,
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </CustomerProvider>
   );
 }
